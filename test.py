a44b4581ba5e8cf94bb501144133fb9c6e4d8c77
Aileen Ouyang <aileenou@umich.edu>
2022-05-15 16:28:17 -0500

    Initial Release minor bug fixes

--- test.py
+++ test.py
@@ -5,3 +5,10 @@ import numpy as np
 import pandas as pd
 import os
 raw_data = pd.read_csv(os.path.join(os.getcwd(), 'cars.csv'))
+# Replace row 0, column Displacement from 307.0 to 0.0
+raw_data.loc[0,'Displacement'] = 0.0
+# Replace row 0, column Car from Chevrolet Chevelle Malibu to rgsg
+raw_data.loc[0,'Car'] = 'rgsg'
+# Export final data
+raw_data = raw_data.to_csv(os.path.join(os.getcwd(),'final.csv'))
+# The End
